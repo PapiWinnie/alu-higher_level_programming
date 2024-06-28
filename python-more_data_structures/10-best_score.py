bdfeb87f90e21cc19137090464b9ec2785a46675
Winston NJi <wnji1@alustudent.com>
2024-06-28 08:01:13 -0700

    add

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,21 +1,5 @@
-
 #!/usr/bin/python3
-
-
 def best_score(a_dictionary):
-    if a_dictionary is None or not isinstance(a_dictionary, dict):
-        return None
-
-    if len(a_dictionary) == 0:
-        return None
-
-    high_val = 0
-    best_key = None
-
-    for k, v in a_dictionary.items():
-        if v > high_val:
-            high_val = v
-            best_key = k
-
-    return best_key
-
+    if not a_dictionary:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
